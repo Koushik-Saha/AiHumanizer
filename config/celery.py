fae2040b79092e60020413aef4fd818fd73f11b4
Koushik Saha <koushik.saha666@gmail.com>
2025-04-04 20:08:08 -0700

    Add celery configuration in project

---
+++ config/celery.py
@@ -0,0 +1,16 @@
+# config/celery.py
+
+import os
+from celery import Celery
+
+# set the default Django settings module for the 'celery' program.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+app = Celery('ai_humanizer')
+
+# Using a string here means the worker will not have to serialize
+# the configuration object to child processes.
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# Load task modules from all registered Django apps.
+app.autodiscover_tasks()
