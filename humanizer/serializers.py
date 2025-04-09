64da9978e68ccea3d9eaafd9541a012aad7bf1cd
Koushik Saha <koushik.saha666@gmail.com>
2025-04-08 20:47:12 -0700

    Update serializer to include detection evasion mode parameter

--- humanizer/serializers.py
+++ humanizer/serializers.py
@@ -2,3 +2,4 @@ from rest_framework import serializers
 
 class HumanizeTextSerializer(serializers.Serializer):
     content = serializers.CharField()
+    detection_evasion = serializers.BooleanField(default=False)
