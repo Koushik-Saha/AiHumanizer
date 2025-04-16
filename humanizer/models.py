227eb5da8dbb5c6cd6450f1cddfcc254fe6ec814
Koushik Saha <koushik.saha666@gmail.com>
2025-04-16 16:38:10 -0700

    Add flagged field to Submission model

--- humanizer/models.py
+++ humanizer/models.py
@@ -25,3 +25,13 @@ class APIKey(models.Model):
 
     def __str__(self):
         return f"{self.name} ({self.key})"
+
+class Submission(models.Model):
+    original_content = models.TextField()
+    humanized_content = models.TextField()
+    detection_evasion = models.BooleanField(default=False)
+    plagiarism_check = models.BooleanField(default=False)
+    plagiarism_score = models.IntegerField(null=True, blank=True)
+    plagiarism_report_url = models.URLField(null=True, blank=True)
+    flagged = models.BooleanField(default=False)           # new field
+    created_at = models.DateTimeField(auto_now_add=True)
