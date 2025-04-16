e480bd006c1d4f8f6a7ce2461235de37b8318111
Koushik Saha <koushik.saha666@gmail.com>
2025-04-16 16:23:13 -0700

    Add plagiarism check flag to serializer

--- humanizer/serializers.py
+++ humanizer/serializers.py
@@ -3,3 +3,4 @@ from rest_framework import serializers
 class HumanizeTextSerializer(serializers.Serializer):
     content = serializers.CharField()
     detection_evasion = serializers.BooleanField(default=False)
+    plagiarism_check = serializers.BooleanField(default=False)
