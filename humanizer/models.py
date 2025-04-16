6f51a2281dec8c2b8bb019c0d8c537d5e0bbc0c3
Koushik Saha <koushik.saha666@gmail.com>
2025-04-16 16:26:42 -0700

    Add Submission model for content history and versioning

--- humanizer/models.py
+++ humanizer/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Submission(models.Model):
+    original_content = models.TextField()
+    humanized_content = models.TextField()
+    detection_evasion = models.BooleanField(default=False)
+    plagiarism_check = models.BooleanField(default=False)
+    plagiarism_score = models.IntegerField(null=True, blank=True)
+    plagiarism_report_url = models.URLField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Submission {self.id}"
