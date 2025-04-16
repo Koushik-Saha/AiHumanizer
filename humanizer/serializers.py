b6cbddaa7cce9c0e85a5854b6f4c24b478d72f87
Koushik Saha <koushik.saha666@gmail.com>
2025-04-16 16:56:06 -0700

    Add language field to serializer for multi-language support

--- humanizer/serializers.py
+++ humanizer/serializers.py
@@ -4,6 +4,7 @@ class HumanizeTextSerializer(serializers.Serializer):
     content = serializers.CharField()
     detection_evasion = serializers.BooleanField(default=False)
     plagiarism_check = serializers.BooleanField(default=False)
+    language = serializers.CharField(default='en')  # new field
 
 class SentenceEditorSerializer(serializers.Serializer):
     content = serializers.CharField()
