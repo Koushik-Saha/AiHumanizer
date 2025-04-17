5748a58b76b0ac98edaca9ac1a9fde58f4ea1b5b
Koushik Saha <koushik.saha666@gmail.com>
2025-04-16 19:52:45 -0700

    Add serializers for user registration and login

--- humanizer/serializers.py
+++ humanizer/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 
 class HumanizeTextSerializer(serializers.Serializer):
     content = serializers.CharField()
@@ -9,3 +10,19 @@ class HumanizeTextSerializer(serializers.Serializer):
 class SentenceEditorSerializer(serializers.Serializer):
     content = serializers.CharField()
     detection_evasion = serializers.BooleanField(default=False)
+
+
+class RegisterSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            password=validated_data['password']
+        )
+        return user
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
